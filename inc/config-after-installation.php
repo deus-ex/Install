0f34c132b4b42e77b0a438649da6e1e647334de7
JAY <j.ilukhor@gmail.com>
2015-04-25 15:09:07 +0100

    Added config-after-installation.php
    Application configuration file after installation

---
+++ inc/config-after-installation.php
@@ -0,0 +1,26 @@
+<?php
+        $config = array(
+          'db_prefix' => 'db_',
+          'db_type' => 'mysql',
+          'db_host' => '127.0.0.1',
+          'db_name' => 'db_installer',
+          'db_user' => 'jencube',
+          'db_pass' => '123456',
+          'db_charset' => 'utf8',
+          'db_collation' => 'utf8_general_ci',
+          'db_engine' => 'MyISAM',
+          'encrypt_key' => '28jdikladu/=-{',
+
+          'cache' => TRUE,
+          'cache_path' => 'C:/xampp/htdocs/jencube/plugins/license-manager/app-folder/cache/',
+
+          'default_lang' => 'en-uk',
+
+          'product_ver' => '1.10.14',
+          'product_name' => 'Jencube App',
+
+          'virtual_path' => 'http://localhost/jencube/plugins/license-manager/app-folder/',
+          'directory_path' => 'C:/xampp/htdocs/jencube/plugins/license-manager/app-folder/',
+
+        );
+ ?>
