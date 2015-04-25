55092e011c8582f2f790cec1d3bd98f75de028b0
JAY <j.ilukhor@gmail.com>
2015-04-25 17:38:26 +0100

    Added language files
    Added en-uk.php and en-us.php

---
+++ install/lang/en-uk.php
@@ -0,0 +1,15 @@
+<?php
+
+	$lang	= array
+	(
+
+		// language details
+		'suffix'          => 'en-uk',
+		'title'           => 'English (UK)',
+		'description'     => 'Native Language of the United Kingdom',
+		'modified'        => '2013-10-29 05:00:30',
+		'author'          => 'Jay',
+
+	);
+
+?>
---
+++ install/lang/en-us.php
@@ -0,0 +1,15 @@
+<?php
+
+	$lang	= array
+	(
+
+		// language details
+		'suffix'          => 'en-us',
+		'title'           => 'English (US)',
+		'description'     => 'Native Language of the United States of America',
+		'modified'        => '2013-10-29 05:00:30',
+		'author'          => 'Jay',
+
+	);
+
+?>
