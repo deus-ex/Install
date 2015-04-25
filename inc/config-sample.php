6c6dc68affb6e93dd5d65c5b90879b409cfdc882
JAY <j.ilukhor@gmail.com>
2015-04-25 15:09:48 +0100

    Added config-sample.php
    Sample application configuration file before installation

---
+++ inc/config-sample.php
@@ -0,0 +1,26 @@
+<?php
+        $config = array(
+          'db_prefix' => '{db-pref}',
+          'db_type' => '{db-type}',
+          'db_host' => '{db-host}',
+          'db_name' => '{db-name}',
+          'db_user' => '{db-user}',
+          'db_pass' => '{db-pass}',
+          'db_charset' => '{db-char}',
+          'db_collation' => '{db-coll}',
+          'db_engine' => '{db-engn}',
+          'encrypt_key' => '{salt-key}',
+
+          'cache' => {cache},
+          'cache_path' => '{cache-path}',
+
+          'default_lang' => '{lang}',
+
+          'product_ver' => '{prod-ver}',
+          'product_name' => '{prod-name}',
+
+          'virtual_path' => '{vir-path}',
+          'directory_path' => '{dir-path}',
+
+        );
+ ?>
